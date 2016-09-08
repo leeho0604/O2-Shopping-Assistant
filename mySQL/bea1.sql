5fecb12542e57eca7a21ae0843cad40100bee739
overdamping <nurichoi94@gmail.com>
2016-09-08 16:38:57 +0900

    sql 파일 이동
    파일 혼동이 없도록 디렉토리 이동

--- mySQL/bea1.sql
+++ mySQL/bea1.sql
@@ -1,9 +1,14 @@
 insert into LOOKS_INFO_TB values(1,'bea1','/pic/look_1.bmp');	#http://www.zara.com/kr/ko/sale/woman/아우터/쇼트-보머-재킷-c692722p3556017.html
 
-insert into PRODUCT_INFO_TB values('0264/067','쇼트 보머 재킷',49000,'지퍼로 여미는 보머 재킷. 긴소매. 루즈핏','Top','/pic/0264_067.bmp');
-insert into PRODUCT_INFO_TB values('2753/101','레이스업 스니커즈',79000,'크림 컬러 스니커즈. 설포와 뒤꿈치 부분에 풀탭 디테일이 있음. 여러 소재와 디테일이 믹스된 디자인. 레이스업 여밈. 밑창은 화이트로 배색됨.','Acc','/pic/2753_101.bmp');
-insert into PRODUCT_INFO_TB values('7688/999','판타지 스트라이프 포플린 셔츠',59000,'셔츠형 칼라가 달린 셔츠. 가슴에 플랩 포켓이 있음. 옆면과 뒷면에 트임이 있음. 밑단이 언발란스한 디자인. 소매를 접을 수 있도록 스트랩이 달림. 뒷면에 셔링이 잡힘.','Top','/pic/7688_999.bmp');
-insert into PRODUCT_INFO_TB values('0881/068','루즈핏 롱 스커트',39000,'발목 길이까지 내려오는 스커트. 허리는 신축성있게 마감됨. 테슬이 달린 끈 디테일이 있음. 내부는 안감 처리됨.','Bottom','/pic/0881_068.bmp');
+insert into PRODUCT_INFO_TB values('0264/067','쇼트 보머 재킷',49000,'지퍼로 여미는 보머 재킷. 긴소매. 루즈핏','Top','/pic/0264_067.bmp','겉감\n주요소재 - 100% 폴리에스터\n디테일 - 98% 폴리에스터, 2% 엘라스탄\n안감\n100% 폴리에스터','/pic/laundry1.png');
+
+insert into PRODUCT_INFO_TB values('2753/101','레이스업 스니커즈',79000,'크림 컬러 스니커즈. 설포와 뒤꿈치 부분에 풀탭 디테일이 있음. 여러 소재와 디테일이 믹스된 디자인. 레이스업 여밈. 밑창은 화이트로 배색됨.','Acc','/pic/2753_101.bmp','겉감\n100% 폴리우레탄\n안감\n100% 폴리우레탄\n밑창\n100% 스타이렌 뷰타다이엔 고무\n안창\n100% 면','/pic/laundry2.png');
+
+insert into PRODUCT_INFO_TB values('7688/999','판타지 스트라이프 포플린 셔츠',59000,'셔츠형 칼라가 달린 셔츠. 가슴에 플랩 포켓이 있음. 옆면과 뒷면에 트임이 있음. 밑단이 언발란스한 디자인. 소매를 접을 수 있도록 스트랩이 달림. 뒷면에 셔링이 잡힘.','Top','/pic/7688_999.bmp','겉감\n100% 면','/pic/laundry1.png');
+
+insert into PRODUCT_INFO_TB values('0881/068','루즈핏 롱 스커트',39000,'발목 길이까지 내려오는 스커트. 허리는 신축성있게 마감됨. 테슬이 달린 끈 디테일이 있음. 내부는 안감 처리됨.','Bottom','/pic/0881_068.bmp','겉감\n100% 비스코스\n안감\n100% 폴리에스터','/pic/laundry1.png');
+
+#하하
 
 insert into LOOKS_PRODUCT_TB values(1,'0264/067');
 insert into LOOKS_PRODUCT_TB values(1,'2753/101');
@@ -12,10 +17,13 @@ insert into LOOKS_PRODUCT_TB values(1,'0881/068');
 
 insert into LOOKS_INFO_TB values(2,'bea1','/pic/look_2.bmp');	#http://www.zara.com/kr/ko/sale/woman/아우터/패턴-보머-재킷-c692722p3377519.html
 
-insert into PRODUCT_INFO_TB values('2364/563','패턴 보머 재킷',89000,'힙까지 내려오는 보머 재킷. 라운드넥. 긴소매. 패턴이 있는 패브릭.','Top','/pic/2364_563.bmp');
-insert into PRODUCT_INFO_TB values('5039/099','스트랩 티셔츠',29000,'짧은 길이의 티셔츠. V넥으로 깊게 파인 스타일. 소매가 없음.','Top','/pic/5039_099.bmp');
-insert into PRODUCT_INFO_TB values('1163/151','배기 팬츠',59000,'지퍼로 여미는 보머 재킷. 긴소매. 루즈핏','Bottom','/pic/1163_151.bmp');
-insert into PRODUCT_INFO_TB values('8133/104','누벅 크로스백',99000,'카멜 컬러의 가죽 가방. 손잡이와 길이 조절이 가능한 어깨끈. 포켓이 달린 안감이 있음. 풀탭이 있는 지퍼로 잠금.','Acc','/pic/8133_104.bmp');
+insert into PRODUCT_INFO_TB values('2364/563','패턴 보머 재킷',89000,'힙까지 내려오는 보머 재킷. 라운드넥. 긴소매. 패턴이 있는 패브릭.','Top','/pic/2364_563.bmp','겉감\n100% 폴리에스터\n안감\n100% 폴리에스터','/pic/laundry1.png');
+
+insert into PRODUCT_INFO_TB values('5039/099','스트랩 티셔츠',29000,'짧은 길이의 티셔츠. V넥으로 깊게 파인 스타일. 소매가 없음.','Top','/pic/5039_099.bmp','겉감\n92% 폴리아미드, 8% 엘라스탄','/pic/laundry1.png');
+
+insert into PRODUCT_INFO_TB values('1163/151','배기 팬츠',59000,'지퍼로 여미는 보머 재킷. 긴소매. 루즈핏','Bottom','/pic/1163_151.bmp','겉감\n100% 면','/pic/laundry3.png');
+
+insert into PRODUCT_INFO_TB values('8133/104','누벅 크로스백',99000,'카멜 컬러의 가죽 가방. 손잡이와 길이 조절이 가능한 어깨끈. 포켓이 달린 안감이 있음. 풀탭이 있는 지퍼로 잠금.','Acc','/pic/8133_104.bmp','겉감\n100% 소가죽\n안감\n100% 폴리에스터','/pic/laundry4.png');
 
 insert into LOOKS_PRODUCT_TB values(2,'2364/563');
 insert into LOOKS_PRODUCT_TB values(2,'5039/099');
@@ -24,10 +32,13 @@ insert into LOOKS_PRODUCT_TB values(2,'8133/104');
 
 insert into LOOKS_INFO_TB values(3,'bea1','/pic/look_3.bmp'); #http://www.zara.com/kr/ko/sale/woman/아우터/이미테이션-레더-점퍼-c692722p3527558.html?ts=1467267005174
 
-insert into PRODUCT_INFO_TB values('3046/023','이미테이션 레더 점퍼',89000,'인조 가죽 점퍼. 지퍼가 있음.','Top','/pic/3046_023.bmp');
-insert into PRODUCT_INFO_TB values('2680/101','하이힐 케이지 샌들',79000,'여러 컬러가 믹스된 하이힐 샌들. 발목에서 버클로 여밈.','Acc','/pic/2680_101.bmp');
-insert into PRODUCT_INFO_TB values('5644/114','스트라이프 쇼트 점프수트',35000,'짧은 길이의 스트라이프 패턴 점프수트. V넥으로 깊게 파인 스타일. 뒷면이 오픈된 디자인. 가느다란 어깨끈이 달림.','Dress','/pic/5644_114.bmp');
-insert into PRODUCT_INFO_TB values('4133/104','체인 스터드 크로스백',59000,'금속 스터드가 달린 크로스백. 다양한 색상이 준비되어 있음. 골드 톤의 금속 장식이 있음. 폼폼 디테일이 있는 체인 숄더 스트랩이 달림. 내부에 포켓이 있음. 덮개에 있는 금속 장식으로 잠금.','Acc','/pic/4133_104.bmp');
+insert into PRODUCT_INFO_TB values('3046/023','이미테이션 레더 점퍼',89000,'인조 가죽 점퍼. 지퍼가 있음.','Top','/pic/3046_023.bmp','겉감\n기본소재\n88% 비스코스,12% 폴리에스터\n코팅\n100% 폴리우레탄\n안감\n100% 폴리에스터','/pic/laundry5.png');
+
+insert into PRODUCT_INFO_TB values('2680/101','하이힐 케이지 샌들',79000,'여러 컬러가 믹스된 하이힐 샌들. 발목에서 버클로 여밈.','Acc','/pic/2680_101.bmp','겉감\n100% 폴리에스터\n안감\n100% 폴리우레탄\n밑창\n100% TPU\n안창\n80% 폴리우레탄, 20% 폴리에스터','/pic/laundry2.png');
+
+insert into PRODUCT_INFO_TB values('5644/114','스트라이프 쇼트 점프수트',35000,'짧은 길이의 스트라이프 패턴 점프수트. V넥으로 깊게 파인 스타일. 뒷면이 오픈된 디자인. 가느다란 어깨끈이 달림.','Dress','/pic/5644_114.bmp','겉감\n100% 폴리에스터\n안감\n100% 폴리에스터','/pic/laundry6.png');
+
+insert into PRODUCT_INFO_TB values('4133/104','체인 스터드 크로스백',59000,'금속 스터드가 달린 크로스백. 다양한 색상이 준비되어 있음. 골드 톤의 금속 장식이 있음. 폼폼 디테일이 있는 체인 숄더 스트랩이 달림. 내부에 포켓이 있음. 덮개에 있는 금속 장식으로 잠금.','Acc','/pic/4133_104.bmp','겉감\n주요소재\n100% 폴리에스터\n부수소재\n100% 폴리우레탄\n안감\n100% 폴리에스터','/pic/laundry4.png');
 
 insert into LOOKS_PRODUCT_TB values(3,'3046/023');
 insert into LOOKS_PRODUCT_TB values(3,'2680/101');
@@ -36,10 +47,13 @@ insert into LOOKS_PRODUCT_TB values(3,'4133/104');
 
 insert into LOOKS_INFO_TB values(4,'bea1','/pic/look_4.bmp'); #http://www.zara.com/kr/ko/sale/woman/아우터/크레이프-블레이저-c692722p3548016.html?ts=1467269151880
 
-insert into PRODUCT_INFO_TB values('2161/257','크레이프 블레이저',99000,'이중으로 라펠이 달린 블레이저. 파이핑 처리된 앞면 포켓 디테일. 소매에 스냅 버튼으로 여미는 트임이 있음. 앞면에서 스냅 버튼으로 여밈.','Top','/pic/2161_257.bmp');
-insert into PRODUCT_INFO_TB values('1165/060','프릴 티셔츠',25000,'어깨끈이 달린 티셔츠. 증앙에 프릴 디테일이 있음. 소매가 없는 라운드넥.','Top','/pic/1165_060.bmp');
-insert into PRODUCT_INFO_TB values('2658/101','플랫폼힐 샌들',79000,'옐로우 컬러의 하이힐 샌들. 스트랩이 교차된 디자인. 발목에서 스트랩 버클로 잠금. 청키 플랫폼힐.','Acc','/pic/2658_101.bmp');
-insert into PRODUCT_INFO_TB values('6726/913','크롭트 데님 팬츠',39000,'9부 길이의 데님 팬츠. 하이 웨이스트. 앞면과 뒷면에 포켓이 있음','Bottom','/pic/6726_913.bmp');
+insert into PRODUCT_INFO_TB values('2161/257','크레이프 블레이저',99000,'이중으로 라펠이 달린 블레이저. 파이핑 처리된 앞면 포켓 디테일. 소매에 스냅 버튼으로 여미는 트임이 있음. 앞면에서 스냅 버튼으로 여밈.','Top','/pic/2161_257.bmp','겉감\n100% 폴리에스터\n안감\n주요 안감\n100% 비스코스\n소매 안감\n95% 폴리에스터,5% 엘라스탄','/pic/laundry7.png');
+
+insert into PRODUCT_INFO_TB values('1165/060','프릴 티셔츠',25000,'어깨끈이 달린 티셔츠. 증앙에 프릴 디테일이 있음. 소매가 없는 라운드넥.','Top','/pic/1165_060.bmp','겉감\n50% 면, 50% 린넨','/pic/laundry6.png');
+
+insert into PRODUCT_INFO_TB values('2658/101','플랫폼힐 샌들',79000,'옐로우 컬러의 하이힐 샌들. 스트랩이 교차된 디자인. 발목에서 스트랩 버클로 잠금. 청키 플랫폼힐.','Acc','/pic/2658_101.bmp','겉감\n100% 폴리우레탄\n안감\n100% 폴리우레탄\n밑창\n100% 에틸렌 비닐 아세테이트\n안창\n100% 폴리우레탄','/pic/laundry2.png');
+
+insert into PRODUCT_INFO_TB values('6726/913','크롭트 데님 팬츠',39000,'9부 길이의 데님 팬츠. 하이 웨이스트. 앞면과 뒷면에 포켓이 있음','Bottom','/pic/6726_913.bmp','겉감\n98% 면, 2% 엘라스탄','/pic/laundry8.png');
 
 insert into LOOKS_PRODUCT_TB values(4,'2161/257');
 insert into LOOKS_PRODUCT_TB values(4,'1165/060');
@@ -48,10 +62,13 @@ insert into LOOKS_PRODUCT_TB values(4,'6726/913');
 
 insert into LOOKS_INFO_TB values(5,'bea1','/pic/look_5.bmp');
 
-insert into PRODUCT_INFO_TB values('8011/215','데님 오버롤',89000,'데님 오버롤. 빈티지 로우 진.','Dress','/pic/8011_215.bmp');
-insert into PRODUCT_INFO_TB values('2180/117','스트라이프 탑',49000,'스트라이프 패턴 탑. 반소매.','Top','/pic/2180_117.bmp');
-insert into PRODUCT_INFO_TB values('2021/117','미드 라이즈 스트라이프 팬츠',59000,'스트라이프 팬츠. 미드 라이즈. 사선 포켓.','Bottom','/pic/2021_117.bmp');
-insert into PRODUCT_INFO_TB values('7705/101','매쉬 엘라스틱 스니커즈',39000,'굽 높이: 3.0cm','Acc','/pic/7705_101.bmp');
+insert into PRODUCT_INFO_TB values('8011/215','데님 오버롤',89000,'데님 오버롤. 빈티지 로우 진.','Dress','/pic/8011_215.bmp','겉감\n100% 면','/pic/laundry8.png');
+
+insert into PRODUCT_INFO_TB values('2180/117','스트라이프 탑',49000,'스트라이프 패턴 탑. 반소매.','Top','/pic/2180_117.bmp','겉감\n주요소재\n95% 폴리에스터,5% 엘라스탄\n디테일\n100% 면','/pic/laundry9.png');
+
+insert into PRODUCT_INFO_TB values('2021/117','미드 라이즈 스트라이프 팬츠',59000,'스트라이프 팬츠. 미드 라이즈. 사선 포켓.','Bottom','/pic/2021_117.bmp','겉감\n95% 폴리에스터, 5% 엘라스탄','/pic/laundry9.png');
+
+insert into PRODUCT_INFO_TB values('7705/101','매쉬 엘라스틱 스니커즈',39000,'굽 높이: 3.0cm','Acc','/pic/7705_101.bmp','겉감\n70% 폴리에스터, 30% 폴리우레탄\n안감\n100% 면\n밑창\n100% 고무\n안창\n100% 폴리우레탄','/pic/laundry2.png');
 
 insert into LOOKS_PRODUCT_TB values(5,'8011/215');
 insert into LOOKS_PRODUCT_TB values(5,'2180/117');
@@ -60,18 +77,15 @@ insert into LOOKS_PRODUCT_TB values(5,'7705/101');
 
 insert into LOOKS_INFO_TB values(6,'bea1','/pic/look_6.bmp');
 
-insert into PRODUCT_INFO_TB values('4174/007','베이직 & 스트라이프 콤비 크롭트 탑',8000,'짧은 길이의 탑. 라운드넥. 소매가 없음.','Top','/pic/4174_007.bmp');
-insert into PRODUCT_INFO_TB values('8803/104','메탈릭 클로징 백팩',39000,'세로 x 가로 x 폭: 34 x 26 x 17cm','Acc','/pic/8803_104.bmp');
-insert into PRODUCT_INFO_TB values('5804/101','주트 디테일 도르세이 슈즈',59000,'누드 컬러의 플랫 샌들. 꼬인 주트 효과를 준 밴드 디테일. 화이트로 배색된 그립솔. 굽 높이:1.6cm','Acc','/pic/5804_101.bmp');
-insert into PRODUCT_INFO_TB values('7901/005','베이직 조깅 팬츠',35000,'긴 길이의 조깅 팬츠. 하이 웨이스트. 옆면에 띠 디테일이 있음.','Bottom','/pic/7901_005.bmp');
+insert into PRODUCT_INFO_TB values('4174/007','베이직 앤 스트라이프 콤비 크롭트 탑',8000,'짧은 길이의 탑. 라운드넥. 소매가 없음.','Top','/pic/4174_007.bmp','겉감\n97% 면, 3% 엘라스탄','/pic/laundry9.png');
+
+insert into PRODUCT_INFO_TB values('8803/104','메탈릭 클로징 백팩',39000,'세로 x 가로 x 폭: 34 x 26 x 17cm','Acc','/pic/8803_104.bmp','겉감\n100% 폴리우레탄\n안감\n100% 폴리에스터','/pic/laundry4.png');
+
+insert into PRODUCT_INFO_TB values('5804/101','주트 디테일 도르세이 슈즈',59000,'누드 컬러의 플랫 샌들. 꼬인 주트 효과를 준 밴드 디테일. 화이트로 배색된 그립솔. 굽 높이:1.6cm','Acc','/pic/5804_101.bmp','겉감\n50% 폴리우레탄, 50% 염소가죽\n안감\n60% 폴리우레탄, 40% 폴리에스터\n밑창\n100% TPU\n안창\n85% 염소가죽, 15% 폴리에스터','/pic/laundry2.png');
+
+insert into PRODUCT_INFO_TB values('7901/005','베이직 조깅 팬츠',35000,'긴 길이의 조깅 팬츠. 하이 웨이스트. 옆면에 띠 디테일이 있음.','Bottom','/pic/7901_005.bmp','겉감\n55% 면, 45% 린넨','/pic/laundry10.png');
 
 insert into LOOKS_PRODUCT_TB values(6,'4174/007');
 insert into LOOKS_PRODUCT_TB values(6,'8803/104');
 insert into LOOKS_PRODUCT_TB values(6,'5804/101');
 insert into LOOKS_PRODUCT_TB values(6,'7901/005');
-
-
-
-
-
-
--- mySQL/bea3.sql
+++ mySQL/bea3.sql
@@ -2,17 +2,17 @@ insert into LOOKS_INFO_TB values (13,'bea3','/pic/look_13.bmp');
 #http://www.zara.com/kr/ko/woman/%EC%98%A4%ED%94%BC%EC%8A%A4%EB%A3%A9/%EB%A6%B4%EB%A0%89%EC%8A%A4%ED%95%8F-%ED%81%AC%EB%A0%88%EC%9D%B4%ED%94%84-%ED%8C%AC%EC%B8%A0-c689010p3649667.html
 
 insert into PRODUCT_INFO_TB values ('7901/249','릴렉스핏 크레이프 팬츠',49000,'릴렉스핏으로 이중 크레이프 처리된 팬츠. 여미는 부분이 보이지 않게 처리됨. 옆면에 포켓이 있음. 뒷면에 포켓이 있음.
-','Bottom','/pic/7901_249.bmp');
+','Bottom','/pic/7901_249.bmp','겉감\n72% 폴리에스터, 21% 비스코스, 7% 엘라스탄\n안감\n100% 폴리에스터','/pic/laundry14.png');
 insert into LOOKS_PRODUCT_TB values (13,'7901/249');
 
-insert into PRODUCT_INFO_TB values ('7673/691','더블 크레이프 블레이저',79000,'더블 블레이저. 옆면에 페이크 포켓이 있음. 1개의 버튼으로 여밈.','Top','/pic/7673_691.bmp');
+insert into PRODUCT_INFO_TB values ('7673/691','더블 크레이프 블레이저',79000,'더블 블레이저. 옆면에 페이크 포켓이 있음. 1개의 버튼으로 여밈.','Top','/pic/7673_691.bmp','겉감\n72% 폴리에스터, 21% 비스코스, 7% 엘라스탄\n안감\n몸통 안감\n100% 아세테이트\n소매 안감\n100% 폴리에스터','/pic/laundry15.png');
 insert into LOOKS_PRODUCT_TB values (13,'7673/691');
 
-insert into PRODUCT_INFO_TB values ('2655/106','리본 스웨터',45000,'보트넥 스웨터. 트임이 있는 소매에 리본이 달림.','Top','/pic/2655_106.bmp');
+insert into PRODUCT_INFO_TB values ('2655/106','리본 스웨터',45000,'보트넥 스웨터. 트임이 있는 소매에 리본이 달림.','Top','/pic/2655_106.bmp','겉감\n68% 아크릴, 30% 폴리에스터, 2% 엘라스탄','/pic/laundry10.png');
 insert into LOOKS_PRODUCT_TB values (13,'2655/106');
 
 insert into PRODUCT_INFO_TB values ('6144/101','라미네이티드 힐 레더 앵클부츠',129000,'하이힐 가죽 앵클부츠. 굽 부분은 골드 톤의 둥근 형태로 마무리됨. 발목 부분이 언발란스한 디자인. 뒤꿈치에서 지퍼로 잠금. 청키 힐. 
-굽 높이: 7.5cm','Acc','/pic/6144_101.bmp');
+굽 높이: 7.5cm','Acc','/pic/6144_101.bmp','겉감\n100% 소가죽\n안감\n80% 폴리우레탄, 20% 폴리에스터\n밑창\n100% 고무\n안창\n100% 염소가죽','/pic/laundry2.png');
 insert into LOOKS_PRODUCT_TB values (13,'6144/101');
 
 
@@ -20,20 +20,19 @@ insert into LOOKS_PRODUCT_TB values (13,'6144/101');
 insert into LOOKS_INFO_TB values(14,'bea3','/pic/look_14.bmp');
 #http://www.zara.com/kr/ko/woman/denim-animal/%ED%8E%98%EC%9D%B4%ED%81%AC-%EB%A0%88%EB%8D%94-%EB%A1%B1-%EC%9E%AC%ED%82%B7-c591001p3648657.html
 
-insert into PRODUCT_INFO_TB values ('3046/227','페이크 레더 롱 재킷',99000,'가죽 느낌의 소재로 제조된 긴 재킷. 지퍼로 잠그는 포켓이 있음. 벨트를 두른 하단. 지퍼가 달린 커프스.','Top','/pic/3046_227.bmp');
+insert into PRODUCT_INFO_TB values ('3046/227','페이크 레더 롱 재킷',99000,'가죽 느낌의 소재로 제조된 긴 재킷. 지퍼로 잠그는 포켓이 있음. 벨트를 두른 하단. 지퍼가 달린 커프스.','Top','/pic/3046_227.bmp','겉감\n기본소재\n88% 비스코스,12% 폴리에스터\n코팅\n100% 폴리우레탄\n안감\n100% 폴리에스터','/pic/laundry5.png');
 insert into LOOKS_PRODUCT_TB values (14,'3046/227');
 
 
-insert into PRODUCT_INFO_TB values ('7779/349','튜브 원피스',59000,'짧은 길이의 펜슬 라인 원피스. 소매가 없음. 스탠드 칼라. 등 부분이 트임.','Dress','/pic/7779_349.bmp');
+insert into PRODUCT_INFO_TB values ('7779/349','튜브 원피스',59000,'짧은 길이의 펜슬 라인 원피스. 소매가 없음. 스탠드 칼라. 등 부분이 트임.','Dress','/pic/7779_349.bmp','겉감\n64% 폴리에스터, 32% 비스코스, 4% 엘라스탄\n안감\n100% 폴리에스터','/pic/laundry1.png');
 insert into LOOKS_PRODUCT_TB values (14,'7779/349');
 
-
 insert into PRODUCT_INFO_TB values ('5104/101','엠보스드 레더 힐 앵클부츠',159000,'블랙 컬러의 가죽 앵클부츠. 뱀피 패턴이 엠보싱 처리됨. 앞코 부분은 포인티드로 마감됨. 발목 안쪽 부분에서 지퍼로 잠금. 
-굽 높이: 5cm','Acc','/pic/5104_101.bmp');
+굽 높이: 5cm','Acc','/pic/5104_101.bmp','겉감\n100% 염소가죽\n안감\n100% TPU\n밑창\n100% 열가소성 고무\n안창\n100% 염소가죽','/pic/laundry2.png');
 insert into LOOKS_PRODUCT_TB values (14,'5104/101');
 
 insert into PRODUCT_INFO_TB values ('4067/304','체인 시티백',179000,'엠보싱 처리된 블랙 백. 체인 디테일. 실버 톤의 금속 장식. 핏 조절이 가능한 숄더 스트랩. 포켓이 달린 안감이 있음.
-세로 x 가로 x 폭: 19 x 30 x 10cm','Acc','/pic/4067_304.bmp');
+세로 x 가로 x 폭: 19 x 30 x 10cm','Acc','/pic/4067_304.bmp','겉감\n가방 손잡이\n100% 철\n주소재\n100% 폴리우레탄\n부수소재\n100% 소가죽\n안감\n100% 폴리에스터','/pic/laundry4.png');
 insert into LOOKS_PRODUCT_TB values (14,'4067/304');
 
 
@@ -42,25 +41,26 @@ insert into LOOKS_PRODUCT_TB values (14,'4067/304');
 insert into LOOKS_INFO_TB values(15,'bea3','/pic/look_15.bmp');
 #http://www.zara.com/kr/ko/woman/free-bird/%EB%8F%84%ED%8B%B0%EB%93%9C-%EC%8A%A4%EC%9C%84%EC%8A%A4-%EC%9B%90%ED%94%BC%EC%8A%A4-c450502p3649543.html
 
-insert into PRODUCT_INFO_TB values ('7521/224','도티드 스위스 원피스',59000,'짧은 길이의 도티드 스위스 원피스. 볼륨감 있는 시스루 슬리브. 뒷면 버튼 여밈.','Dress','/pic/7521_224.bmp');
+insert into PRODUCT_INFO_TB values ('7521/224','도티드 스위스 원피스',59000,'짧은 길이의 도티드 스위스 원피스. 볼륨감 있는 시스루 슬리브. 뒷면 버튼 여밈.','Dress','/pic/7521_224.bmp','겉감\n100% 폴리에스터\n안감\n100% 폴리에스터','/pic/laundry16.png');
 insert into LOOKS_PRODUCT_TB values (15,'7521/224');
 
-insert into PRODUCT_INFO_TB values ('6318/223','페이트 스웨이드 재킷',99000,'인조 스웨이드 재킷. 지퍼로 여미는 포켓이 있음. 벨트를 두른 하단. 지퍼가 달린 커프스.','Top','/pic/6318_223.bmp');
+insert into PRODUCT_INFO_TB values ('6318/223','페이크 스웨이드 재킷',99000,'인조 스웨이드 재킷. 지퍼로 여미는 포켓이 있음. 벨트를 두른 하단. 지퍼가 달린 커프스.','Top','/pic/6318_223.bmp','겉감\n100% 폴리에스터\n안감\n96% 폴리에스터, 4% 엘라스탄','/pic/laundry15.png');
 insert into LOOKS_PRODUCT_TB values (15,'6318/223');
 
 insert into LOOKS_PRODUCT_TB values (15,'5104/101');
 
-insert into PRODUCT_INFO_TB values ('8006/104','콤비 레더 버킷백',129000,'높이 x 넓이 x 깊이: 34 x 24 x 16 cm.','Acc','/pic/8006_104.bmp');
+insert into PRODUCT_INFO_TB values ('8006/104','콤비 레더 버킷백',129000,'높이 x 넓이 x 깊이: 34 x 24 x 16 cm.','Acc','/pic/8006_104.bmp','겉감\n100% 소가죽','/pic/laundry4.png');
 insert into LOOKS_PRODUCT_TB values (15,'8006/104');
 
 
 
 insert into LOOKS_INFO_TB values(16,'bea3','/pic/look_16.bmp');
 #http://www.zara.com/kr/ko/woman/free-bird/3-4-%EC%8A%AC%EB%A6%AC%EB%B8%8C-%ED%94%84%EB%A6%B4-%EB%B8%94%EB%9D%BC%EC%9A%B0%EC%8A%A4-c450502p3702030.html
-insert into PRODUCT_INFO_TB values ('9878/152','3/4 슬리브 프릴 블라우스',49000,'짧은 길이의 시스루 루즈핏 블라우스. 하이넥. 프릴이 있음. 뒷면에서 여밈.','Top','/pic/9878_152.bmp');
+
+insert into PRODUCT_INFO_TB values ('9878/152','3/4 슬리브 프릴 블라우스',49000,'짧은 길이의 시스루 루즈핏 블라우스. 하이넥. 프릴이 있음. 뒷면에서 여밈.','Top','/pic/9878_152.bmp','겉감\n100% 폴리에스터','/pic/laundry3.png');
 insert into LOOKS_PRODUCT_TB values (16,'9878/152');
 
-insert into PRODUCT_INFO_TB values ('8228/227','롤업 미드 라이즈 스키니핏 진',59000,'키니 라인의 미드 라이즈 진. 포켓이 5개 있음. 밑단이 셀비지 유형으로 재봉되어 접힌 디자인.','Bottom','/pic/8228_227.bmp');
+insert into PRODUCT_INFO_TB values ('8228/227','롤업 미드 라이즈 스키니핏 진',59000,'키니 라인의 미드 라이즈 진. 포켓이 5개 있음. 밑단이 셀비지 유형으로 재봉되어 접힌 디자인.','Bottom','/pic/8228_227.bmp','겉감\n98% 면, 2% 엘라스탄','/pic/laundry9.png');
 insert into LOOKS_PRODUCT_TB values (16,'8228/227');
 
 insert into LOOKS_PRODUCT_TB values (16,'5104/101');
@@ -71,14 +71,15 @@ insert into LOOKS_PRODUCT_TB values (16,'5104/101');
 
 insert into LOOKS_INFO_TB values(17,'bea3','/pic/look_17.bmp');
 #http://www.zara.com/kr/ko/woman/%EC%9D%B4%EB%B2%88-%EC%A3%BC-%EC%8B%A0%EC%83%81%ED%92%88/%ED%81%AC%EB%A1%AD%ED%8A%B8-%EC%A0%90%ED%94%84%EC%88%98%ED%8A%B8-c840002p3746028.html
-insert into PRODUCT_INFO_TB values ('0264/223','크롭트 점프수트',49000,'','Dress','/pic/0264_223.bmp');
+
+insert into PRODUCT_INFO_TB values ('0264/223','크롭트 점프수트',49000,' ','Dress','/pic/0264_223.bmp','겉감\n상단\n97% 폴리에스터, 3% 엘라스탄\n하단\n100% 비스코스','/pic/laundry1.png');
 insert into LOOKS_PRODUCT_TB values (17,'0264/223');
 
 
 insert into LOOKS_PRODUCT_TB values (17,'5104/101');
 
 insert into PRODUCT_INFO_TB values ('8005/104','엠보스드 버킷백',79000,'블랙 컬러 버킷백 스타일의 가방. 애니멀 패턴으로 엠보싱 처리된 디자인. 내부에 수납공간이 3개 있음. 가운데 수납 공간은 지퍼로 잠금. 자석으로 잠금. 손잡이와 탈부착이 가능한 어깨끈. 
-세로 x 가로 x 폭: 29 x 24 x 19cm','Acc','/pic/8005_104.bmp');
+세로 x 가로 x 폭: 29 x 24 x 19cm','Acc','/pic/8005_104.bmp','겉감\n100% 폴리우레탄\n안감\n100% 폴리에스터','/pic/laundry4.png');
 insert into LOOKS_PRODUCT_TB values (17,'8005/104');
 
 
@@ -88,10 +89,10 @@ insert into LOOKS_PRODUCT_TB values (17,'8005/104');
 insert into LOOKS_INFO_TB values(18,'bea3','/pic/look_18.bmp');
 #http://www.zara.com/kr/ko/woman/free-bird/%EB%B3%BC%EB%A5%A8-%EB%B8%94%EB%9D%BC%EC%9A%B0%EC%8A%A4-c450502p3649534.html
 
-insert into PRODUCT_INFO_TB values ('7200/221','볼륨 블라우스',49000,'라운드넥 블라우스. 긴소매. 커프스와 네크라인에 디테일이 있음. 뒷면에서 여밈.','Top','/pic/7200_221.bmp');
+insert into PRODUCT_INFO_TB values ('7200/221','볼륨 블라우스',49000,'라운드넥 블라우스. 긴소매. 커프스와 네크라인에 디테일이 있음. 뒷면에서 여밈.','Top','/pic/7200_221.bmp','겉감\n100% 폴리에스터','/pic/laundry9.png');
 insert into LOOKS_PRODUCT_TB values (18,'7200/221');
 
-insert into PRODUCT_INFO_TB values ('3564/220','하이웨이스트 캐롯핏 팬츠',59000,'하이 웨이스트 팬츠. 벨트 고리. 앞면에 플리츠 주름이 있음. 옆면에 포켓이 있음. ','Bottom','/pic/3564_220.bmp');
+insert into PRODUCT_INFO_TB values ('3564/220','하이웨이스트 캐롯핏 팬츠',59000,'하이 웨이스트 팬츠. 벨트 고리. 앞면에 플리츠 주름이 있음. 옆면에 포켓이 있음. ','Bottom','/pic/3564_220.bmp','겉감\n64% 비스코스, 36% 폴리에스터','/pic/laundry3.png');
 insert into LOOKS_PRODUCT_TB values (18,'3564/220');
 
 insert into LOOKS_PRODUCT_TB values (18,'5104/101');
